d4d6ffcb2ae5942a20bdbc53563cca1cd4c09a19
yibochen <chenyibo.zjumath@gmail.com>
2012-06-01 11:28:38 +0800

    修改计算词频的算法

--- f_weibo_getweibo_web.r
+++ f_weibo_getweibo_web.r
@@ -208,6 +208,7 @@ unlist(strsplit(mmseg4j(x), ' '))
 words <- unlist(mapply(f_cut, weibo_data))
 words <- words[words != 'na']
 words <- words[words != '转发']
+names(words) <- NULL
 
 # 统计词频
 words_freq <- sort(table(words), dec=T)
@@ -218,8 +219,9 @@ words_length <- nchar(words_names)
 SogouLabDic <- read.table('SogouLabDic.dic', fill=T, head=F)
 
 words_df <- data.frame(words_names=words_names, words_freq=words_freq, words_length=words_length)
-# 只做两个字的词，简单一点。。。
-words_df <- words_df[words_df$words_length == 2, ]
+# 只做两三个字的词，简单一点。。。
+words_df <- words_df[words_df$words_length %in% c(2,3), ]
+words_df[, 1] <- as.character(words_df[, 1])
 names(SogouLabDic)[1] <- 'words_names'
 SogouLabDic <- SogouLabDic[SogouLabDic[,1] %in% words_df$words_names, ]
 
@@ -227,29 +229,19 @@ words_df2 <- merge (words_df, SogouLabDic, by='words_names', all.x=T)
 # words_df2 <- words_df2[grep('^[NV],',words_df2$V3), ]
 # 可以筛选名词和动词。不过似乎没有必要，因为形容词副词什么的也能够体现用词风格嘛
 words_df2 <- words_df2[order(-words_df2[,2]), ]
+words_df2$V2[is.na(words_df2$V2)] <- mean(SogouLabDic$V2)
+words_df2$words_freq2 <- words_df2$words_freq/words_df2$V2
+words_df3 <- words_df2[words_df2$words_freq2 > median(words_df2$words_freq2), c('words_names', 'words_freq2')]
 
-words_df3 <- words_df2[is.na(words_df2$V2), ]
-words_df4 <- words_df2[!is.na(words_df2$V2), ]
-
-# 匹配不到的，用原来的词频。匹配到的，就看是否超过平均水平
-words_df4$words_freq2 <- words_df4$words_freq/words_df4$V2
-words_df4 <- words_df4[words_df4$words_freq2 > mean(words_df4$words_freq2), 1:2]
-
-words_df5 <- rbind(words_df4, words_df3[, 1:2])
-
-# # 手动删除一些虚词（这里没找到算法实现）
-# quantile(words_freq, seq(0,1,0.05))
-# # write.csv(words_names[words_length == 2 & words_freq>=25], 'c2.csv')
-# # 此处手动处理
-# badwords <- read.csv('c2.csv', head=T, as.is=T)[, 2]
-# words_freq <- words_freq[words_length>=2 & !(words_names %in% badwords)]
-# words_names <- names(words_freq)
+words_df3 <- words_df3[order(-words_df3$words_freq2), ][1:50, ]
+words_df3$words_rank <- ceiling(rank(words_df3$words_freq2))
+words_df3$words_rank <- ceiling(words_df3$words_rank*50/max(words_df3$words_rank))
 
 # 做词云（这个包貌似对中文支持不是很好）
 png(paste('weibo_wordcloud_', Sys.Date(), '_', hisnick, '.png', sep=''),width=500,height=500)
 par(mar=c(0,0,0,0))
-wordcloud(words_df5$words_names, words_df5$words_freq, min.freq=2, 
-scale=c(8,1), max.words=50, random.order=F, colors=terrain.colors(50,1))
+wordcloud(words_df3$words_names, words_df3$words_rank, 
+scale=c(4,1), max.words=50, random.order=F, colors=terrain.colors(50,1))
 dev.off()
 }
 
